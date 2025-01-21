3da0395889ab5fbb047b3e26ed6f490a3b1d03e2
Emmanuelf21 <emmanuelfranco2001@gmail.com>
2025-01-21 18:56:47 -0300

    adicionei os card de habilidades

--- src/pages/Habilidades.jsx
+++ src/pages/Habilidades.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import './habilidades.css';
 
 const Habilidades = () => {
   return (
     <div className="container-habilidades">
+      <section className='habilidades'>
         <h2>Habilidades</h2>
+        <span className='cards-habilidades'>
+          <div className='card-habilidade'></div>
+          <div className='card-habilidade'></div>
+          <div className='card-habilidade'></div>
+        </span>
+      </section>
     </div>
   )
 }
