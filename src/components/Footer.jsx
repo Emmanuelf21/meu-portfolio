f599a24bba699c846652f20bec99b94c2e81b4d3
Emmanuelf21 <emmanuelfranco2001@gmail.com>
2025-01-28 18:56:46 -0300

    arrumando a home para mobile

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,7 +10,7 @@ const footer = () => {
     <footer>
       
         <p className='email'>
-          <AiOutlineMail /> emmanuelfranco2001@gmail.com
+          <AiOutlineMail /> <p>emmanuelfranco2001@gmail.com</p>
         </p>
       
       <div className='social-media'>
