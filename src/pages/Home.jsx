3700d1976655650d3573423237cdd6807222cafd
Emmanuelf21 <emmanuelfranco2001@gmail.com>
2026-08-25 12:53:37 -0300

    alteração na home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Foto from '../assets/images/Emmanuel_Franco_Desenvolvedor_Frontend.jpeg'
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 import './home.css'
 
@@ -9,21 +9,24 @@ const Home = () => {
     <div className="container-home">
       <div className="home-sobre">
         <h2>
-          Muito prazer!<br/>
-          <span><p>Eu sou o</p> <p className='texto-azul text2'>Emmanuel Franco!</p></span>
+          Muito prazer!<br />
+          <span><p>Me chamo</p> <p className='texto-azul text2'>Emmanuel Franco!</p></span>
         </h2>
         <p>
-          Bacharel em Ciências da computação e cursando Técnico em Desenvolvimento de Sistemas.
-          <br/>
-          <br/>
-          Entusiasta de tecnologia e gosto de criar experiências digitais intuitivas.
-          <br/>
-          <br/>
-          Explore meu portfólio para saber mais do meu trabalho!
+          Desenvolvedor Front-end Júnior.
+          <br />
+          <br />
+          Desenvolvo aplicações web modernas, responsivas e focadas em resolver problemas reais, utilizando React, TypeScript e APIs.
+          <br />
+          <br />
+          Confira meus projetos e veja na prática como transformo ideias em aplicações funcionais.
         </p>
-        <Link to="https://drive.google.com/file/d/1nwRQsVKfyid7839_TYKPtbTzM9fR7nPB/view?usp=sharing" className='botao' target='_Blank'>
-          Download CV
-        </Link>
+        <nav className='nav-home'>
+          <Link to="projetos?q=3" className='h2-projetos' >Ver Projetos</Link>
+          <Link to="https://drive.google.com/file/d/1nwRQsVKfyid7839_TYKPtbTzM9fR7nPB/view?usp=sharing" className='botao' target='_Blank'>
+            Download CV
+          </Link>
+        </nav>
       </div>
       <div className="container-foto">
         <img src={Foto} alt="" />
